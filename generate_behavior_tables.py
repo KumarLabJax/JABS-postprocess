54ac5307b6aeea0b9f9fda22c15cae71714c1287
Brian Geuther <Brian.Geuther@jax.org>
2022-12-13 10:26:16 -0500

    First full version of this script for generating behavior tables Needs a code refactor to split off functions, but can be used to produce tables

--- generate_behavior_tables.py
+++ generate_behavior_tables.py
@@ -140,8 +140,50 @@ def read_experiment_folder(folder: os.path, behavior: str):
 	return all_predictions
 
 # Transforms raw data per-experiment into binned results
-def generate_binned_results(df: pd.DataFrame, bin_size_frames: int=108000):
-	raise NotImplementedError
+def generate_binned_results(df: pd.DataFrame, bin_size_minutes: int=60):
+	grouped_df = df.groupby(['exp_prefix','longterm_idx'])
+	all_results = []
+	for cur_group, cur_data in grouped_df:
+		time_data = to_vector(cur_data)
+		binned_results = time_data.groupby(pd.Grouper(freq=str(bin_size_minutes) + 'T'), group_keys=True).apply(get_results).reset_index().drop(columns=['level_1'])
+		binned_results['exp_prefix'], binned_results['longterm_idx'] = cur_group
+		all_results.append(binned_results)
+	all_results = pd.concat(all_results)
+	return all_results
+
+# Modifies a block of time to contain the predictions
+def get_results(x):
+	y = pd.DataFrame()
+	y['time_no_pred'] = [np.sum(x['behavior']==-1)]
+	y['time_not_behavior'] = [np.sum(x['behavior']==0)]
+	y['time_behavior'] = [np.sum(x['behavior']==1)]
+	# bout_no_pred = [np.sum(x['bout']==-1)]
+	# bout_not_behavior = [np.sum(x['bout']==0)]
+	y['bout_behavior'] = [np.sum(x['bout']==1)]
+	return y
+
+# Moves clock to the next hour
+def add_hour(t):
+	# Rounds to nearest hour by adding a timedelta hour if minute >= 30
+	return (t.replace(day=t.day+(t.hour+1)//24, second=0, microsecond=0, minute=0, hour=(t.hour+1)%24))
+
+# Converts an event dataframe into a vector
+# This function should only be run on a single animal (eg 1 prediction per-frame)
+def to_vector(event_df: pd.DataFrame):
+	vid_blocks = event_df.groupby('time')
+	dfs = []
+	for cur_block, cur_group in vid_blocks:
+		end_time = datetime.strftime(add_hour(datetime.strptime(cur_block,'%Y-%m-%d %H:%M:%S')), '%Y-%m-%d %H:%M:%S')
+		time_idx = pd.date_range(start=cur_block, end=end_time, freq='33ms')
+		time_vector = np.zeros(len(time_idx), dtype=np.int8)-1
+		bout_vector = np.zeros(len(time_idx), dtype=np.float32)
+		for _, cur_row in cur_group.iterrows():
+			time_vector[cur_row['start']:cur_row['start']+cur_row['duration']] = cur_row['is_behavior']
+			if cur_row['is_behavior'] == 1:
+				bout_vector[cur_row['start']:cur_row['start']+cur_row['duration']] = 1/cur_row['duration']
+		dfs.append(pd.DataFrame({'behavior':time_vector, 'bout':bout_vector}, index=time_idx))
+	df = pd.concat(dfs)
+	return df
 
 # Generates a dictionary of dictionaries to link identities between files
 # First layer of dictionaries is the file being translated
@@ -237,30 +279,46 @@ def hungarian_match_ids(group1, group2):
 	return row_best, col_best
 
 # Writes the header of filers used in this script to file
-def write_experiment_header(out_file: os.path, args):
-	raise NotImplementedError
+def write_experiment_header(out_file: os.path, args, behavior):
+	header_df = pd.DataFrame({'Project Folder': [args.project_folder], 'Behavior': [behavior], 'Stitch Gap': [args.stitch_gap], 'Min Bout Length': [args.min_bout_length], 'Out Bin Size': [args.out_bin_size]})
+	if os.path.exists(out_file) and not args.overwrite:
+		raise FileExistsError('Out_file ' + str(out_file) + ' exists. Please use --overwrite if you wish to overwrite data.')
+	else:
+		with open(out_file, 'w') as f:
+			header_df.to_csv(f, header=True, index=False)
 
-def write_experiment_bouts():
-	raise NotImplementedError
+# Writes the experiment data to file with a header
+def write_experiment_data(args, behavior, bout_table, suffix=None):
+	if suffix is None:
+		suffix = ''
+	out_file = args.out_prefix + suffix + '.csv'
+	try:
+		# Write the header
+		write_experiment_header(out_file, args, behavior)
+		# Write the data
+		with open(out_file, 'a') as f:
+			bout_table.to_csv(f, header=True, index=False)
+	except FileExistsError:
+		pass
 
-def write_experiment_binned():
-	raise NotImplementedError
-
+# Generates the 2 prediction tables given an experiment
 def generate_behavior_tables(args, behavior: str):
 	# Detect all the experiments in a folder
 	exp_folders = get_experiments_in_folder(args.project_folder)
 	# Read in all the experiments (RLE format)
-	all_experiment_data = []
+	experiment_bout_data = []
 	for cur_experiment in exp_folders:
 		experiment_data = read_experiment_folder(cur_experiment, behavior)
-		all_experiment_data.append(experiment_data)
+		experiment_bout_data.append(experiment_data)
 	# Merge experiments into a single project (RLE format)
-	all_experiment_data = pd.concat(all_experiment_data)
-	# TODO:
+	experiment_bout_data = pd.concat(experiment_bout_data)
 	# Write project bout output
+	write_experiment_data(args, behavior, experiment_bout_data, suffix='_bouts')
 	# Convert project into binned data
+	experiment_bin_data = generate_binned_results(experiment_bout_data, args.out_bin_size)
 	# Write binned project output
-	return all_experiment_data
+	write_experiment_data(args, behavior, experiment_bin_data, suffix='_summaries')
+	return experiment_bout_data
 
 def main(argv):
 	parser = argparse.ArgumentParser(description='Script that transforms JABS behavior predictions for a project folder into an easier to work with set of files.')
@@ -269,7 +327,8 @@ def main(argv):
 	parser.add_argument('--min_bout_length', help='Minimum number of frames in which a behavior prediction must be to be considered, default=5', default=5, type=int)
 	parser.add_argument('--out_bin_size', help='Time duration used in binning the results, default=60', default=60, type=int)
 	parser.add_argument('--behavior', help='Behavior to produce a table for, default=all behaviors in project folder', default=None, type=str)
-	parser.add_argument('--out_prefix', help='File prefix to write output tables (prefix_bouts.csv and prefix_summaries.csv), default=behavior', default=None, type=str)
+	parser.add_argument('--out_prefix', help='File prefix to write output tables (prefix_bouts.csv and prefix_summaries.csv), default=behavior', default=None, type=str, required=True)
+	parser.add_argument('--overwrite', help='Overwrites output files, default=False', default=False, action='store_true')
 	#
 	args = parser.parse_args()
 	# Detect or select which behavior we want tables for
