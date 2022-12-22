Fixing IoU calculation and adding bout plots

analysis_utils/gt_utils.py
@@ -3,56 +3,57 @@ import numpy as np
 # Calculates the intersection of 2 bouts
 # Each bout is a tuple of (start, duration)
 def calculate_intersection(gt_bout: tuple[int, int], pr_bout: tuple[int, int]):
-    # Detect the larger of the 2 start times
-    max_start_time = np.max([gt_bout[0], pr_bout[0]])
-    # Detect the smaller of the 2 end times
-    gt_bout_end = gt_bout[0]+gt_bout[1]
-    pr_bout_end = pr_bout[0]+pr_bout[1]
-    min_end_time = np.min([gt_bout_end, pr_bout_end])
-    # Detect if the 2 bouts intersected at all
-    if max_start_time < min_end_time:
-        return min_end_time-max_start_time
-    else:
-        return 0
-    
+	# Detect the larger of the 2 start times
+	max_start_time = np.max([gt_bout[0], pr_bout[0]])
+	# Detect the smaller of the 2 end times
+	gt_bout_end = gt_bout[0]+gt_bout[1]
+	pr_bout_end = pr_bout[0]+pr_bout[1]
+	min_end_time = np.min([gt_bout_end, pr_bout_end])
+	# Detect if the 2 bouts intersected at all
+	if max_start_time < min_end_time:
+		return min_end_time-max_start_time
+	else:
+		return 0
+	
 # Calculates the union of 2 bouts
 # Each bout is a tuple of (start, duration)
 def calculate_union(gt_bout: tuple[int, int], pr_bout: tuple[int, int]):
-    # If the 2 don't intersect, we can just sum up the durations
-    if calculate_intersection(gt_bout, pr_bout) == 0:
-        return gt_bout[1] + pr_bout[1]
-    # They do intersect
-    else:
-        min_start_time = np.min([gt_bout[0], pr_bout[0]])
-        gt_bout_end = gt_bout[0]+gt_bout[1]
-        pr_bout_end = pr_bout[0]+pr_bout[1]
-        max_end_time = np.max([gt_bout_end, pr_bout_end])
-        return max_end_time - min_start_time
+	# If the 2 don't intersect, we can just sum up the durations
+	if calculate_intersection(gt_bout, pr_bout) == 0:
+		return gt_bout[1] + pr_bout[1]
+	# They do intersect
+	else:
+		min_start_time = np.min([gt_bout[0], pr_bout[0]])
+		gt_bout_end = gt_bout[0]+gt_bout[1]
+		pr_bout_end = pr_bout[0]+pr_bout[1]
+		max_end_time = np.max([gt_bout_end, pr_bout_end])
+		return max_end_time - min_start_time
 
 # Generates an IoU matrix based on bout arrays
 def get_iou_mat(gt_bouts: np.array, pr_bouts: np.array):
-    intersection_mat = np.zeros([len(gt_bouts), len(pr_bouts)])
-    union_mat = np.zeros([len(gt_bouts), len(pr_bouts)])
-    # Calculate each of the intersection/unions
-    for gt_idx, pr_idx in zip(np.arange(len(gt_bouts)), np.arange(len(pr_bouts))):
-        cur_gt_bout = tuple(gt_bouts[gt_idx,:])
-        cur_pr_bout = tuple(pr_bouts[pr_idx,:])
-        # Calculate the intersections, unions, and IoUs
-        intersection_mat[gt_idx, pr_idx] = calculate_intersection(cur_gt_bout, cur_pr_bout)
-        union_mat[gt_idx, pr_idx] = calculate_union(cur_gt_bout, cur_pr_bout)
-    iou_mat = np.divide(intersection_mat, union_mat, out=np.zeros_like(intersection_mat), where=union_mat!=0)
-    return intersection_mat, union_mat, iou_mat
+	intersection_mat = np.zeros([len(gt_bouts), len(pr_bouts)])
+	union_mat = np.zeros([len(gt_bouts), len(pr_bouts)])
+	# Calculate each of the intersection/unions
+	for gt_idx in np.arange(len(gt_bouts)):
+		for pr_idx in np.arange(len(pr_bouts)):
+			cur_gt_bout = tuple(gt_bouts[gt_idx,:])
+			cur_pr_bout = tuple(pr_bouts[pr_idx,:])
+			# Calculate the intersections, unions, and IoUs
+			intersection_mat[gt_idx, pr_idx] = calculate_intersection(cur_gt_bout, cur_pr_bout)
+			union_mat[gt_idx, pr_idx] = calculate_union(cur_gt_bout, cur_pr_bout)
+	iou_mat = np.divide(intersection_mat, union_mat, out=np.zeros_like(intersection_mat), where=union_mat!=0)
+	return intersection_mat, union_mat, iou_mat
 
 # Define detection metrics, given a IoU threshold
 def calc_temporal_iou_metrics(iou_data, threshold):
-    tp_counts = 0
-    fn_counts = 0
-    fp_counts = 0
-    for cur_iou_mat in iou_data:
-        tp_counts += np.sum(np.any(cur_iou_mat>threshold, axis=1))
-        fn_counts += np.sum(np.all(cur_iou_mat<threshold, axis=1))
-        fp_counts += np.sum(np.all(cur_iou_mat<threshold, axis=0))
-    precision = tp_counts/(tp_counts + fp_counts)
-    recall = tp_counts/(tp_counts + fn_counts)
-    f1 = 2*(precision * recall)/(precision + recall)
-    return precision, recall, f1
+	tp_counts = 0
+	fn_counts = 0
+	fp_counts = 0
+	for cur_iou_mat in iou_data:
+		tp_counts += np.sum(np.any(cur_iou_mat>threshold, axis=1))
+		fn_counts += np.sum(np.all(cur_iou_mat<threshold, axis=1))
+		fp_counts += np.sum(np.all(cur_iou_mat<threshold, axis=0))
+	precision = tp_counts/(tp_counts + fp_counts)
+	recall = tp_counts/(tp_counts + fn_counts)
+	f1 = 2*(precision * recall)/(precision + recall)
+	return precision, recall, f1

compare_gt.py
@@ -18,12 +18,19 @@ gt_annotations = rutils.read_project_annotations(gt_annotations_folder)
 predictions_folder = '/media/bgeuther/Storage/TempStorage/leinani_social_behavior_classifiers/exp_v5_poses/'
 pred_behaviors = putils.get_behaviors_in_folder(predictions_folder)
 pred_poses = putils.get_poses_in_folder(predictions_folder)
+
+# Leinani tuned the filters per-behavior
+behavior_filters = {'Approach':5, 'Chase':9, 'Leave':3, 'Nose_genital':9, 'Nose_nose':9}
 predictions = []
 for behavior in pred_behaviors:
 	for pose_file in pred_poses:
 		cur_video = putils.pose_to_video(pose_file)
 		# Read in only the first 2 minutes of the predictions
-		prediction_df = rutils.parse_predictions(putils.pose_to_prediction(pose_file, behavior), stitch_bouts=5, filter_bouts=5, trim_time=(0,60*30*2))
+		if behavior in behavior_filters.keys():
+			bout_filter = behavior_filters[behavior]
+		else:
+			bout_filter = 5
+		prediction_df = rutils.parse_predictions(putils.pose_to_prediction(pose_file, behavior), stitch_bouts=5, filter_bouts=bout_filter, trim_time=(0,60*30*2))
 		prediction_df['behavior'] = behavior
 		prediction_df['video'] = cur_video
 		prediction_df = prediction_df[prediction_df['is_behavior']==1]
@@ -47,6 +54,10 @@ for cur_behavior, tmp_df in all_annotations.groupby('behavior'):
 	for cur_animal, animal_df in tmp_df.groupby('mouse_idx'):
 		# For each animal, we want a matrix of intersections, unions, and ious
 		gt_bouts = animal_df[animal_df['is_gt']][['start','duration']].values
+		# Also filter the gt based on Leinani's filters
+		if cur_behavior in behavior_filters.keys():
+			gt_bouts = animal_df[animal_df['is_gt']]
+			gt_bouts = gt_bouts[gt_bouts['duration']>=behavior_filters[cur_behavior]][['start','duration']].values
 		pr_bouts = animal_df[~animal_df['is_gt']][['start','duration']].values
 		int_mat, u_mat, iou_mat = gutils.get_iou_mat(gt_bouts, pr_bouts)
 		iou_list.append(iou_mat)
@@ -58,6 +69,7 @@ for cur_behavior, tmp_df in all_annotations.groupby('behavior'):
 performance_df = pd.concat(performance_df)
 performance_df = pd.melt(performance_df, id_vars=['behavior','threshold'])
 
+# Plot the performance
 (
 	p9.ggplot(performance_df, p9.aes(x='threshold', y='value', color='variable'))+
 	p9.geom_line()+
@@ -65,4 +77,21 @@ performance_df = pd.melt(performance_df, id_vars=['behavior','threshold'])
 	p9.facet_wrap('~behavior')+
 	p9.labs(x='IoU Threshold', y='Performance', color='Metric')+
 	p9.scale_color_brewer(type='qual', palette='Set1')
+).draw().show()
+
+# Plot the bouts
+all_annotations['end'] = all_annotations['start']+all_annotations['duration']
+factor_mouse = pd.factorize(all_annotations['mouse_idx'])
+all_annotations['yax'] = factor_mouse[0]
+
+
+(
+	p9.ggplot(all_annotations[all_annotations['behavior']=='Leave'])+
+	p9.geom_rect(p9.aes(xmin='start', xmax='end', ymin='yax + is_gt/2', ymax='yax + is_gt/2 + 0.5', fill='is_gt'))+
+	p9.theme_bw()+
+	p9.facet_wrap('~behavior')+
+	p9.scale_y_continuous(breaks=np.arange(len(factor_mouse[1]))+0.5, labels=factor_mouse[1])+
+	p9.scale_fill_brewer(type='qual', palette='Set1')+
+	p9.geom_hline(p9.aes(yintercept='y'), pd.DataFrame({'y':np.arange(len(factor_mouse[1])+1)}))+
+	p9.labs(x='Frame', fill='GT?')
 ).draw().show()