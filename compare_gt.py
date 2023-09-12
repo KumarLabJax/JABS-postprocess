c36ce91fd1491d4109205d86030cc07048b60871
Brian Geuther <Brian.Geuther@jax.org>
2023-09-12 14:19:55 -0400

    stash edits for play analysis

--- compare_gt.py
+++ compare_gt.py
@@ -1,160 +1,296 @@
+"""Associated lines of code that deal with the comparison of predictions (from classify.py) and GT annotation (from a JABS project)."""
+
 import pandas as pd
 import plotnine as p9
 import re
 import numpy as np
-from itertools import chain
 
-from analysis_utils.parse_table import read_ltm_summary_table, filter_experiment_time
-from analysis_utils.plots import generate_time_vs_feature_plot
 import jabs_utils.read_utils as rutils
 import jabs_utils.project_utils as putils
 import analysis_utils.gt_utils as gutils
 
 # Read in the gt annotations
-gt_annotations_folder = '/media/bgeuther/Storage/TempStorage/leinani_social_behavior_classifiers/exported_gt/'
+# gt_annotations_folder = '/media/bgeuther/Storage/TempStorage/leinani_social_behavior_classifiers/exported_gt/'
+gt_annotations_folder = "."
 gt_annotations = rutils.read_project_annotations(gt_annotations_folder)
 
 # Read in the gt predictions
-predictions_folder = '/media/bgeuther/Storage/TempStorage/leinani_social_behavior_classifiers/exp_v5_poses/'
+# predictions_folder = '/media/bgeuther/Storage/TempStorage/leinani_social_behavior_classifiers/exp_v5_poses/'
+predictions_folder = "."
 pred_behaviors = putils.get_behaviors_in_folder(predictions_folder)
 pred_poses = putils.get_poses_in_folder(predictions_folder)
 
+# trim_time = (0,60*30*2)
+trim_time = None
+
 # Leinani tuned the filters per-behavior
-behavior_filters = {'Approach':5, 'Chase':9, 'Leave':3, 'Nose_genital':9, 'Nose_nose':9}
+# behavior_filters = {'Approach': {'filter': 5}, 'Chase': {'filter': 9}, 'Leave': {'filter': 3}, 'Nose_genital': {'filter': 9}, 'Nose_nose': {'filter': 9}}
+# Play behaviors
+behavior_filters = {
+    "Chase": {"filter": 37, "stitch": 7},
+    "Jerk": {"filter": 5, "stitch": 3},
+}
+default_filter = 5
+default_stitch = 5
+
 predictions = []
 for behavior in pred_behaviors:
-	for pose_file in pred_poses:
-		cur_video = putils.pose_to_video(pose_file)
-		# Read in only the first 2 minutes of the predictions
-		if behavior in behavior_filters.keys():
-			bout_filter = behavior_filters[behavior]
-		else:
-			bout_filter = 5
-		prediction_df = rutils.parse_predictions(putils.pose_to_prediction(pose_file, behavior), stitch_bouts=5, filter_bouts=bout_filter, trim_time=(0,60*30*2))
-		prediction_df['behavior'] = behavior
-		prediction_df['video'] = cur_video
-		prediction_df = prediction_df[prediction_df['is_behavior']==1]
-		predictions.append(prediction_df)
+    for pose_file in pred_poses:
+        cur_video = putils.pose_to_video(pose_file)
+        # Select filter/stitch for behavior
+        if behavior in behavior_filters.keys():
+            if "filter" in behavior_filters[behavior].keys():
+                bout_filter = behavior_filters[behavior]["filter"]
+            else:
+                bout_filter = default_filter
+            if "stitch" in behavior_filters[behavior].keys():
+                bout_stitch = behavior_filters[behavior]["stitch"]
+            else:
+                bout_stitch = default_stitch
+        # Use a default
+        else:
+            bout_filter = default_filter
+            bout_stitch = default_stitch
+        prediction_df = rutils.parse_predictions(
+            putils.pose_to_prediction(pose_file, behavior),
+            stitch_bouts=bout_stitch,
+            filter_bouts=bout_filter,
+            trim_time=trim_time,
+        )
+        prediction_df["behavior"] = behavior
+        prediction_df["video"] = cur_video
+        prediction_df = prediction_df[prediction_df["is_behavior"] == 1]
+        predictions.append(prediction_df)
 
 predictions = pd.concat(predictions).reset_index(drop=True)
 
 # Combine the df to make comparing animals a lot easier
-gt_annotations['is_gt'] = True
-predictions['is_gt'] = False
+gt_annotations["is_gt"] = True
+predictions["is_gt"] = False
 all_annotations = pd.concat([gt_annotations, predictions])
-all_annotations['behavior'] = [re.sub('-','_',x) for x in all_annotations['behavior']]
-all_annotations['mouse_idx'] = all_annotations['video'] + '_' + all_annotations['animal_idx'].astype(str)
-all_annotations['mouse_idx'] = all_annotations['mouse_idx'].astype('category')
+all_annotations["behavior"] = [re.sub("-", "_", x) for x in all_annotations["behavior"]]
+all_annotations["mouse_idx"] = (
+    all_annotations["video"] + "_" + all_annotations["animal_idx"].astype(str)
+)
+all_annotations["mouse_idx"] = all_annotations["mouse_idx"].astype("category")
 
 # Calculate performance metrics
 performance_df = []
 # Loop over the animals by behavior
-for cur_behavior, tmp_df in all_annotations.groupby('behavior'):
-	iou_list = []
-	for cur_animal, animal_df in tmp_df.groupby('mouse_idx'):
-		# For each animal, we want a matrix of intersections, unions, and ious
-		gt_bouts = animal_df[animal_df['is_gt']][['start','duration']].values
-		# Also filter the gt based on Leinani's filters
-		if cur_behavior in behavior_filters.keys():
-			gt_bouts = animal_df[animal_df['is_gt']]
-			gt_bouts = gt_bouts[gt_bouts['duration']>=behavior_filters[cur_behavior]][['start','duration']].values
-		pr_bouts = animal_df[~animal_df['is_gt']][['start','duration']].values
-		int_mat, u_mat, iou_mat = gutils.get_iou_mat(gt_bouts, pr_bouts)
-		iou_list.append(iou_mat)
-	# For each behavior, we can scan the thresholds for performances
-	for threshold in np.arange(0.05,1.01,0.05):
-		precision, recall, f1 = gutils.calc_temporal_iou_metrics(iou_list, threshold)
-		performance_df.append(pd.DataFrame({'behavior':[cur_behavior], 'threshold':[threshold], 'precision':[precision], 'recall':[recall], 'f1':[f1]}))
+for cur_behavior, tmp_df in all_annotations.groupby("behavior"):
+    iou_list = []
+    for cur_animal, animal_df in tmp_df.groupby("mouse_idx"):
+        # For each animal, we want a matrix of intersections, unions, and ious
+        gt_bouts = animal_df[animal_df["is_gt"]][["start", "duration"]].values
+        # Also filter the gt based on Leinani's filters
+        if cur_behavior in behavior_filters.keys():
+            gt_bouts = animal_df[animal_df["is_gt"]]
+            gt_bouts = gt_bouts[
+                gt_bouts["duration"] >= behavior_filters[cur_behavior]["filter"]
+            ][["start", "duration"]].values
+        pr_bouts = animal_df[~animal_df["is_gt"]][["start", "duration"]].values
+        int_mat, u_mat, iou_mat = gutils.get_iou_mat(gt_bouts, pr_bouts)
+        iou_list.append(iou_mat)
+    # For each behavior, we can scan the thresholds for performances
+    for threshold in np.arange(0.05, 1.01, 0.05):
+        precision, recall, f1 = gutils.calc_temporal_iou_metrics(iou_list, threshold)
+        performance_df.append(
+            pd.DataFrame(
+                {
+                    "behavior": [cur_behavior],
+                    "threshold": [threshold],
+                    "precision": [precision],
+                    "recall": [recall],
+                    "f1": [f1],
+                }
+            )
+        )
 
 performance_df = pd.concat(performance_df)
-performance_df = pd.melt(performance_df, id_vars=['behavior','threshold'])
+performance_df = pd.melt(performance_df, id_vars=["behavior", "threshold"])
 
 # Plot the performance
 (
-	p9.ggplot(performance_df, p9.aes(x='threshold', y='value', color='variable'))+
-	p9.geom_line()+
-	p9.theme_bw()+
-	p9.facet_wrap('~behavior')+
-	p9.labs(x='IoU Threshold', y='Performance', color='Metric')+
-	p9.scale_color_brewer(type='qual', palette='Set1')
+    p9.ggplot(performance_df, p9.aes(x="threshold", y="value", color="variable"))
+    + p9.geom_line()
+    + p9.theme_bw()
+    + p9.facet_wrap("~behavior")
+    + p9.labs(x="IoU Threshold", y="Performance", color="Metric")
+    + p9.scale_color_brewer(type="qual", palette="Set1")
 ).draw().show()
 
 # Plot the bouts
-all_annotations['end'] = all_annotations['start']+all_annotations['duration']
-factor_mouse = pd.factorize(all_annotations['mouse_idx'])
-all_annotations['yax'] = factor_mouse[0]
+all_annotations["end"] = all_annotations["start"] + all_annotations["duration"]
+factor_mouse = pd.factorize(all_annotations["mouse_idx"])
+all_annotations["yax"] = factor_mouse[0]
 
 
 (
-	p9.ggplot(all_annotations[all_annotations['behavior']=='Leave'])+
-	p9.geom_rect(p9.aes(xmin='start', xmax='end', ymin='yax + is_gt/2', ymax='yax + is_gt/2 + 0.5', fill='is_gt'))+
-	p9.theme_bw()+
-	p9.facet_wrap('~behavior')+
-	p9.scale_y_continuous(breaks=np.arange(len(factor_mouse[1]))+0.5, labels=factor_mouse[1])+
-	p9.scale_fill_brewer(type='qual', palette='Set1')+
-	p9.geom_hline(p9.aes(yintercept='y'), pd.DataFrame({'y':np.arange(len(factor_mouse[1])+1)}))+
-	p9.labs(x='Frame', fill='GT?')
+    p9.ggplot(all_annotations)
+    + p9.geom_rect(
+        p9.aes(
+            xmin="start",
+            xmax="end",
+            ymin="yax + is_gt/2",
+            ymax="yax + is_gt/2 + 0.5",
+            fill="is_gt",
+        )
+    )
+    + p9.theme_bw()
+    + p9.facet_grid("behavior~.")
+    + p9.scale_y_continuous(
+        breaks=np.arange(len(factor_mouse[1])) + 0.5, labels=factor_mouse[1]
+    )
+    + p9.scale_fill_brewer(type="qual", palette="Set1")
+    + p9.geom_hline(
+        p9.aes(yintercept="y"), pd.DataFrame({"y": np.arange(len(factor_mouse[1]) + 1)})
+    )
+    + p9.labs(x="Frame", fill="GT?")
 ).draw().show()
 
 
 # Parameter tuning for filter/stitches
 prediction_tuning = []
-stitches_to_test = [3, 5, 7, 9, 11, 13, 15]
-filters_to_test = [3, 5, 7, 9, 11, 13, 15]
+stitches_to_test = [
+    3,
+    5,
+    7,
+    9,
+    11,
+    13,
+    15,
+    17,
+    19,
+    21,
+    23,
+    25,
+    27,
+    29,
+    31,
+    33,
+    35,
+    37,
+    39,
+    41,
+    43,
+    45,
+]
+filters_to_test = [
+    3,
+    5,
+    7,
+    9,
+    11,
+    13,
+    15,
+    17,
+    19,
+    21,
+    23,
+    25,
+    27,
+    29,
+    31,
+    33,
+    35,
+    37,
+    39,
+    41,
+    43,
+    45,
+]
 for behavior in pred_behaviors:
-	for pose_file in pred_poses:
-		cur_video = putils.pose_to_video(pose_file)
-		for cur_stitch in stitches_to_test:
-			for cur_filter in filters_to_test:
-				# Read in only the first 2 minutes of the predictions
-				prediction_df = rutils.parse_predictions(putils.pose_to_prediction(pose_file, behavior), stitch_bouts=cur_stitch, filter_bouts=cur_filter, trim_time=(0,60*30*2))
-				prediction_df['behavior'] = behavior
-				prediction_df['video'] = cur_video
-				prediction_df['stitch'] = cur_stitch
-				prediction_df['filter'] = cur_filter
-				prediction_df = prediction_df[prediction_df['is_behavior']==1]
-				prediction_tuning.append(prediction_df)
+    for pose_file in pred_poses:
+        cur_video = putils.pose_to_video(pose_file)
+        for cur_stitch in stitches_to_test:
+            for cur_filter in filters_to_test:
+                # Read in only the first 2 minutes of the predictions
+                prediction_df = rutils.parse_predictions(
+                    putils.pose_to_prediction(pose_file, behavior),
+                    stitch_bouts=cur_stitch,
+                    filter_bouts=cur_filter,
+                    trim_time=trim_time,
+                )
+                prediction_df["behavior"] = behavior
+                prediction_df["video"] = cur_video
+                prediction_df["stitch"] = cur_stitch
+                prediction_df["filter"] = cur_filter
+                prediction_df = prediction_df[prediction_df["is_behavior"] == 1]
+                prediction_tuning.append(prediction_df)
 
 prediction_tuning = pd.concat(prediction_tuning).reset_index(drop=True)
 
-gt_annotations['is_gt'] = True
-prediction_tuning['is_gt'] = False
+gt_annotations["is_gt"] = True
+prediction_tuning["is_gt"] = False
 tuning_annotations = pd.concat([gt_annotations, prediction_tuning])
-tuning_annotations['behavior'] = [re.sub('-','_',x) for x in tuning_annotations['behavior']]
-tuning_annotations['mouse_idx'] = tuning_annotations['video'] + '_' + tuning_annotations['animal_idx'].astype(str)
-tuning_annotations['mouse_idx'] = tuning_annotations['mouse_idx'].astype('category')
+tuning_annotations["behavior"] = [
+    re.sub("-", "_", x) for x in tuning_annotations["behavior"]
+]
+tuning_annotations["mouse_idx"] = (
+    tuning_annotations["video"] + "_" + tuning_annotations["animal_idx"].astype(str)
+)
+tuning_annotations["mouse_idx"] = tuning_annotations["mouse_idx"].astype("category")
 
 performance_df = []
 # Loop over the animals by behavior
-for cur_behavior, tmp_df in tuning_annotations.groupby('behavior'):
-	tuning_iou_list = []
-	for cur_animal, animal_df in tmp_df.groupby('mouse_idx'):
-		# For each animal, we want a matrix of intersections, unions, and ious
-		gt_bouts = animal_df[animal_df['is_gt']][['start','duration']].values
-		# Cycle over the new stitch + filter metrics
-		for grp, pr_df in animal_df[~animal_df['is_gt']].groupby(['stitch','filter']):
-			pr_bouts = pr_df[~pr_df['is_gt']][['start','duration']].values
-			int_mat, u_mat, iou_mat = gutils.get_iou_mat(gt_bouts, pr_bouts)
-			tuning_iou_list.append({'stitch':grp[0], 'filter':grp[1], 'iou':iou_mat})
-	# For each behavior, we can scan the thresholds for performances
-	for threshold in np.arange(0.05,1.01,0.05):
-		# Different combinations
-		combinations = np.array(np.meshgrid(stitches_to_test, filters_to_test)).T.reshape([-1,2])
-		for cur_combo in combinations:
-			iou_list = [x['iou'] for x in tuning_iou_list if x['stitch']==cur_combo[0] and x['filter']==cur_combo[1]]
-			precision, recall, f1 = gutils.calc_temporal_iou_metrics(iou_list, threshold)
-			performance_df.append(pd.DataFrame({'behavior':[cur_behavior], 'threshold':[threshold], 'precision':[precision], 'recall':[recall], 'f1':[f1], 'stitch':[cur_combo[0]], 'filter':[cur_combo[1]]}))
+for cur_behavior, tmp_df in tuning_annotations.groupby("behavior"):
+    tuning_iou_list = []
+    for cur_animal, animal_df in tmp_df.groupby("mouse_idx"):
+        # For each animal, we want a matrix of intersections, unions, and ious
+        gt_bouts = animal_df[animal_df["is_gt"]][["start", "duration"]].values
+        # Cycle over the new stitch + filter metrics
+        for grp, pr_df in animal_df[~animal_df["is_gt"]].groupby(["stitch", "filter"]):
+            pr_bouts = pr_df[~pr_df["is_gt"]][["start", "duration"]].values
+            int_mat, u_mat, iou_mat = gutils.get_iou_mat(gt_bouts, pr_bouts)
+            tuning_iou_list.append({"stitch": grp[0], "filter": grp[1], "iou": iou_mat})
+    # For each behavior, we can scan the thresholds for performances
+    for threshold in np.arange(0.05, 1.01, 0.05):
+        # Different combinations
+        combinations = np.array(
+            np.meshgrid(stitches_to_test, filters_to_test)
+        ).T.reshape([-1, 2])
+        for cur_combo in combinations:
+            iou_list = [
+                x["iou"]
+                for x in tuning_iou_list
+                if x["stitch"] == cur_combo[0] and x["filter"] == cur_combo[1]
+            ]
+            precision, recall, f1 = gutils.calc_temporal_iou_metrics(
+                iou_list, threshold
+            )
+            performance_df.append(
+                pd.DataFrame(
+                    {
+                        "behavior": [cur_behavior],
+                        "threshold": [threshold],
+                        "precision": [precision],
+                        "recall": [recall],
+                        "f1": [f1],
+                        "stitch": [cur_combo[0]],
+                        "filter": [cur_combo[1]],
+                    }
+                )
+            )
 
 performance_df = pd.concat(performance_df)
-performance_df = pd.melt(performance_df, id_vars=['behavior','threshold','stitch','filter'])
+performance_df = pd.melt(
+    performance_df, id_vars=["behavior", "threshold", "stitch", "filter"]
+)
 
 # Plot the performance
 (
-	p9.ggplot(performance_df[np.logical_and(performance_df['variable']=='f1',performance_df['threshold']==0.5)], p9.aes(x='stitch', y='filter', fill='value'))+
-	p9.geom_tile(p9.aes(width=2, height=2), color='black')+
-	p9.geom_text(p9.aes(label='np.round(value,2)'))+
-	p9.theme_bw()+
-	p9.facet_wrap('~behavior')+
-	p9.labs(x='Stitch', y='Filter', fill='F1 @ 0.5 IoU')+
-	p9.scale_color_brewer(type='qual', palette='Set1')
+    p9.ggplot(
+        performance_df[
+            np.logical_and(
+                performance_df["variable"] == "f1", performance_df["threshold"] == 0.5
+            )
+        ],
+        p9.aes(x="stitch", y="filter", fill="value"),
+    )
+    + p9.geom_tile(p9.aes(width=2, height=2), color="black")
+    + p9.geom_text(p9.aes(label="np.round(value,2)"))
+    + p9.theme_bw()
+    + p9.facet_wrap("~behavior")
+    + p9.labs(x="Stitch", y="Filter", fill="F1 @ 0.5 IoU")
+    + p9.scale_color_brewer(type="qual", palette="Set1")
 ).draw().show()
--- test_plot.py
+++ test_plot.py
@@ -2,6 +2,7 @@ import pandas as pd
 import plotnine as p9
 import re
 import numpy as np
+import mizani
 
 from analysis_utils.parse_table import read_ltm_summary_table, filter_experiment_time
 from analysis_utils.plots import generate_time_vs_feature_plot
@@ -10,11 +11,19 @@ from analysis_utils.plots import generate_time_vs_feature_plot
 # Example multi-day summary plot
 #-------------------------------
 # Read in the summary results
-results_file = '/media/bgeuther/Storage/TempStorage/test-behavior-project/ProjectResults_2022-12-19_Approach_summaries.csv'
+results_file = 'Results_2023-01-13_Approach_summaries.csv'
 
 jmcrs_data = '~/Downloads/2022-11-16 TOM_TotalQueryForConfluence.xlsx'
-header_data, df = read_ltm_summary_table(results_file, jmcrs_metadata=jmcrs_data)
 
+def read_adjusted_data(file, jmcrs):
+	header_data, df = read_ltm_summary_table(file, jmcrs_metadata=jmcrs)
+	# Delete out bins where no data exists
+	no_data = np.all(df[['time_no_pred','time_not_behavior','time_behavior']]==0, axis=1)
+	df = df[~no_data]
+	df['Behavior'] = header_data['Behavior'][0]
+	return header_data, df
+
+header_data, df = read_adjusted_data(results_file, jmcrs_data)
 # Now we have a dataframe with a bunch of temporal columns and a bunch of feature columns
 df.keys()
 
@@ -34,23 +43,102 @@ generate_time_vs_feature_plot(filter_experiment_time(df), 'zt_time_hour', 'bout_
 filter_experiment_time(df).groupby('Strain').agg({'rel_time_behavior':np.mean})
 
 # Read in more behaviors to make an aggregate graph
+# Nose behaviors don't look good, but are present here
 # This is stretching the limits of the function capability. Facets don't work too well. Nonetheless, plot room by behavior predictions
-df['Behavior'] = header_data['Behavior'][0]
-results_file2 = '/media/bgeuther/Storage/TempStorage/test-behavior-project/ProjectResults_2022-12-19_Nose_nose_summaries.csv'
-header_data, df2 = read_ltm_summary_table(results_file2, jmcrs_metadata=jmcrs_data)
-df2['Behavior'] = header_data['Behavior'][0]
-results_file3 = '/media/bgeuther/Storage/TempStorage/test-behavior-project/ProjectResults_2022-12-19_Nose_genital_summaries.csv'
-header_data, df3 = read_ltm_summary_table(results_file3, jmcrs_metadata=jmcrs_data)
-df3['Behavior'] = header_data['Behavior'][0]
-results_file4 = '/media/bgeuther/Storage/TempStorage/test-behavior-project/ProjectResults_2022-12-19_Chase_summaries.csv'
-header_data, df4 = read_ltm_summary_table(results_file4, jmcrs_metadata=jmcrs_data)
-df4['Behavior'] = header_data['Behavior'][0]
-results_file5 = '/media/bgeuther/Storage/TempStorage/test-behavior-project/ProjectResults_2022-12-19_Leave_summaries.csv'
-header_data, df5 = read_ltm_summary_table(results_file5, jmcrs_metadata=jmcrs_data)
-df5['Behavior'] = header_data['Behavior'][0]
-df = pd.concat([df, df2, df3, df4, df5])
+results_file2 = 'Results_2023-01-13_Nose_nose_summaries.csv'
+header_data, df2 = read_adjusted_data(results_file2, jmcrs_data)
+results_file3 = 'Results_2023-01-13_Nose_genital_summaries.csv'
+header_data, df3 = read_adjusted_data(results_file3, jmcrs_data)
+results_file4 = 'Results_2023-01-13_Chase_summaries.csv'
+header_data, df4 = read_adjusted_data(results_file4, jmcrs_data)
+results_file5 = 'Results_2023-01-13_Leave_summaries.csv'
+header_data, df5 = read_adjusted_data(results_file5, jmcrs_data)
+results_file6 = 'Results_2023-01-13_Activity_1_summaries.csv'
+header_data, df6 = read_adjusted_data(results_file6, jmcrs_data)
+# Heuristic classifiers
+results_file7 = 'Results_2023-02-07_heuristic_approach_summaries.csv'
+header_data, df7 = read_adjusted_data(results_file7, jmcrs_data)
+results_file8 = 'Results_2023-02-07_heuristic_close_summaries.csv'
+header_data, df8 = read_adjusted_data(results_file8, jmcrs_data)
+results_file9 = 'Results_2023-02-07_heuristic_contact_summaries.csv'
+header_data, df9 = read_adjusted_data(results_file9, jmcrs_data)
+results_file10 = 'Results_2023-02-07_heuristic_nose_ear_summaries.csv'
+header_data, df10 = read_adjusted_data(results_file10, jmcrs_data)
+results_file11 = 'Results_2023-02-07_heuristic_nose_genital_summaries.csv'
+header_data, df11 = read_adjusted_data(results_file11, jmcrs_data)
+results_file12 = 'Results_2023-02-07_heuristic_nose_nose_summaries.csv'
+header_data, df12 = read_adjusted_data(results_file12, jmcrs_data)
+# Filtering tests
+results_file12 = 'results/Results_2023-02-07_heuristic_nose_nose_summaries.csv'
+header_data, df12 = read_adjusted_data(results_file12, jmcrs_data)
+results_file13 = 'filtered_results/test_2023-02-13_highact_heuristic_nose_nose_summaries.csv'
+header_data, df13 = read_adjusted_data(results_file13, jmcrs_data)
+results_file14 = 'filtered_results/test_2023-02-13_medact_heuristic_nose_nose_summaries.csv' # Filtered by 5cm/s
+header_data, df14 = read_adjusted_data(results_file14, jmcrs_data)
+results_file15 = 'filtered_results/test_2023-02-13_heuristic_nose_nose_summaries.csv' # filtered by 2.5cm/s
+header_data, df15 = read_adjusted_data(results_file15, jmcrs_data)
+results_file16 = 'filtered_results/test_2023-02-13_medact_invover_heuristic_nose_nose_summaries.csv' # Filtered by 5cm/s
+header_data, df16 = read_adjusted_data(results_file16, jmcrs_data)
+# df = pd.concat([df, df2, df3, df4, df5, df6])
+df = pd.concat([df, df4, df5, df6])
+df2 = pd.concat([df2, df3])
+df3 = pd.concat([df7,df8,df9,df10,df11,df12])
+df4 = pd.concat([df12,df13,df14,df15,df16])
 
-(generate_time_vs_feature_plot(df, 'zt_exp_time', 'rel_time_behavior')+p9.facet_grid('Behavior~Room', scales='free_y')).draw().show()
+(generate_time_vs_feature_plot(filter_experiment_time(df[df['Behavior']!='Activity > 2.5cm/s']), 'zt_time_hour', 'rel_time_behavior')+p9.facet_grid('Behavior~Room', scales='free_y')).draw().show()
+(generate_time_vs_feature_plot(filter_experiment_time(df[df['Behavior']=='Activity > 2.5cm/s']), 'zt_time_hour', 'behavior_dist')+p9.facet_grid('Behavior~Room', scales='free_y')).draw().show()
+(generate_time_vs_feature_plot(filter_experiment_time(df2), 'zt_time_hour', 'rel_time_behavior')+p9.facet_grid('Behavior~Room', scales='free_y')).draw().show()
+
+(generate_time_vs_feature_plot(filter_experiment_time(df4), 'zt_time_hour', 'bout_behavior', 'Behavior')+p9.facet_grid('Strain~Room', scales='free_y')).draw().show()
+
+# Plots for Vivek (re-generating Gautam's) for social paper/presentation
+# bars are SE
+# data is remove for habituation, but we can just ignore day1 for this...
+behaviors = ['Approach', 'Chase', 'Leave']
+bout_df = filter_experiment_time(df[df['Behavior'].isin(behaviors)])
+activity_df = filter_experiment_time(df[df['Behavior']=='Activity > 5.0cm/s'])
+# Lazy to put the features into the same column
+bout_df.loc[:,'feature_vals'] = bout_df['bout_behavior']
+activity_df.loc[:,'feature_vals'] = activity_df['behavior_dist']
+full_df = pd.concat([bout_df, activity_df])
+# Base plot that doesn't scale everything correctly...
+# (generate_time_vs_feature_plot(filter_experiment_time(full_df), 'zt_time_hour', 'feature_vals', 'Strain')+p9.facet_grid('Behavior~Room', scales='free_y')).draw().show()
+
+from matplotlib import gridspec
+import matplotlib.pyplot as plt
+
+all_panels = []
+panel_strs = behaviors + ['Activity > 5.0cm/s']
+rooms = {'B2B': 'B2B', 'CBAX2B': 'CBAX2', 'B6': 'RAF-B6'}
+for idx in range(len(panel_strs)):
+	panel_df = full_df[full_df['Behavior'] == panel_strs[idx]]
+	panel_light = panel_df.groupby(['zt_time_hour','Strain'])[['feature_vals','lights_on']].mean().reset_index().groupby('zt_time_hour')[['feature_vals','lights_on']].max().reset_index()
+	panel_light['lights_val'] = (1-panel_light['lights_on'])*1.2*np.max(panel_light['feature_vals'])
+	for key, val in rooms.items():
+		sub_df = panel_df[panel_df['Room']==key]
+		panel = (
+			p9.ggplot(sub_df) +
+			p9.geom_bar(p9.aes(x='zt_time_hour', y='lights_val'), panel_light, width=1, stat='identity', fill='lightgrey') + 
+			p9.stat_summary(p9.aes(x='zt_time_hour', y='feature_vals', color='Strain', fill='Strain'), fun_ymin=lambda x: np.mean(x)-np.std(x)/np.sqrt(len(x)), fun_ymax=lambda x: np.mean(x)+np.std(x)/np.sqrt(len(x)), fun_y=np.mean, geom=p9.geom_smooth) + 
+			p9.stat_summary(p9.aes(x='zt_time_hour', y='feature_vals', color='Strain', fill='Strain'), fun_y=np.mean, geom=p9.geom_point) + 
+			# p9.facet_grid('.~Room') + 
+			p9.theme_bw() + 
+			p9.scale_color_brewer(type='qual', palette='Set1') + 
+			p9.scale_fill_brewer(type='qual', palette='Set1', guide=False)
+		)
+		if panel_strs[idx] == 'Activity > 5.0cm/s':
+			panel = panel + p9.labs(title=val, color='Strain', x='ZT Time', y='Distance Traveled (cm)')
+		else:
+			panel = panel + p9.labs(title='', color='Strain', x='ZT Time', y=panel_strs[idx] + ' Counts')
+		all_panels.append(panel)
+
+fig = (p9.ggplot()+p9.geom_blank(data=full_df)+p9.theme_void()).draw()
+gs = gridspec.GridSpec(int(len(all_panels)/len(rooms)),len(rooms))
+for idx in range(len(all_panels)):
+	cur_ax = fig.add_subplot(gs[idx//len(rooms),idx%len(rooms)])
+	_ = all_panels[idx]._draw_using_figure(fig, [cur_ax])
+
+fig.show()
 
 #----------------------
 # Example Ethogram plot
@@ -63,10 +151,12 @@ df_bouts['time'] = pd.to_datetime(df_bouts['time'])
 frame_to_s = 30
 df_bouts['time_start'] = df_bouts['time'] + pd.to_timedelta(df_bouts['start']/frame_to_s, unit='s')
 df_bouts['time_end'] = df_bouts['time'] + pd.to_timedelta(df_bouts['start']/frame_to_s, unit='s') + pd.to_timedelta(df_bouts['duration']/frame_to_s, unit='s')
+experiments = np.unique(df_bouts['exp_prefix'])
+
 
 # Plot the bouts (behavior, not behavior, and missing data)
 (
-	p9.ggplot(df_bouts)+
+	p9.ggplot(df_bouts[df_bouts['exp_prefix']==experiments[0]])+
 	p9.geom_rect(p9.aes(xmin='time_start', xmax='time_end', ymin='longterm_idx - 0.5', ymax='longterm_idx + 0.5', fill='factor(is_behavior)'))+
 	p9.scale_x_datetime(breaks=mizani.breaks.date_breaks('6 hour'), labels=mizani.formatters.date_format('%Y-%m-%d %H-%M-%S'))+
 	p9.scale_fill_discrete(labels=['No Mouse', 'Not Behavior', 'Behavior'])+
