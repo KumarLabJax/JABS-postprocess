1176cf00b41187484cd3dcd622233494d450b5d0
Brian Geuther <Brian.Geuther@jax.org>
2023-09-25 11:08:38 -0400

    Updating qc plot, reporting relative to low counts, and adding a comment on generating video list

--- analysis_utils/qc_plots.py
+++ analysis_utils/qc_plots.py
@@ -6,6 +6,8 @@ import time
 from datetime import datetime
 
 folder = '/media/bgeuther/Storage/TempStorage/SocialPaper/Play/analysis-2023-07-20/juveniles/'
+# These lists were generated via video names that exist on dropbox
+# /media/bgeuther/Storage/TempStorage/B6-BTBR$ cat all_files_2023-08-14.txt | grep -E '\.avi$|_pose_est_v5\.h5$' | awk '{print $2}' | sed -e 's:\.avi::' -e 's:_pose_est_v5\.h5::' | uniq | sed -e 's:$:_pose_est_v5.h5:' | grep -E $pattern > /media/bgeuther/Storage/TempStorage/SocialPaper/Play/analysis-2023-07-20/juveniles/adult_male_list.txt 
 flist = np.loadtxt(folder + 'juvenile_list.txt', dtype=str)
 df = pd.DataFrame({'fname': flist, 'dataset': 'juvenile'})
 
@@ -19,73 +21,12 @@ df['exp'], df['day'], df['time'], _, _, df['pose_v'] = np.split(np.array([row['p
 df['hour'], df['minute'], df['second'] = np.split(np.array([row['time'].split('-') for _, row in df.iterrows()]), 3, axis=-1)
 
 # How uniform is the data loss?
-df.groupby(['dataset','exp']).apply(len)
-# dataset       exp    
-    # adult_female  MDX0063    97
-    #               MDX0064    97
-    #               MDX0065    96
-    #               MDX0066    96
-    #               MDX0067    96
-    #               MDX0068    97
-    #               MDX0069    97
-    #               MDX0089    95
-    #               MDX0091    95
-    #               MDX0092    94
-    #               MDX0093    92
-    # adult_male    MDB0003     1 --
-    #               MDB0004    94
-    #               MDB0007    97
-    #               MDB0008    96
-    #               MDB0009    94
-    #               MDB0010    99
-    #               MDB0011    62 --
-    #               MDB0012    93
-    #               MDB0013    97
-    #               MDB0014    98
-    #               MDB0015    98
-    #               MDB0047    92
-    #               MDB0048    94
-    #               MDB0049    88 --
-    #               MDB0050    92
-    #               MDB0051    94
-    #               MDB0052    89
-    #               MDB0053    96
-    #               MDB0054    96
-    #               MDX0005    94
-    #               MDX0006    97
-    #               MDX0007    83 --
-    #               MDX0008    98
-    #               MDX0009    95
-    #               MDX0010    93
-    #               MDX0012    92
-    #               MDX0013    96
-    #               MDX0014    92
-    #               MDX0017    31 --
-    #               MDX0018    96
-    # juvenile      MDB0284    93
-    #               MDB0285    93
-    #               MDB0288    93
-    #               MDB0289    94
-    #               MDB0290    92
-    #               MDB0292    95
-    #               MDB0293    95
-    #               MDB0297    86 --
-    #               MDB0298    97
-    #               MDB0300    94
-    #               MDB0301    93
-    #               MDB0302    94
-    #               MDB0303    94
-    #               MDB0304    95
-    #               MDB0305    92
-    #               MDB0313    92
-    #               MDX0653    95
-    #               MDX0654    81 --
-
+# df.groupby(['dataset','exp']).apply(len)
 
 # QA Reporting
 # This qa was generated via (run in the total log folder):
-# mlr --csv unsparsify *.csv > qa_2023-09-11.csv
-qa = pd.read_csv(folder + 'qa_2023-09-11.csv')
+# mlr --csv unsparsify *.csv > qa_2023-09-25.csv.csv
+qa = pd.read_csv(folder + 'qa_2023-09-25.csv')
 # cloud fname only contains the last 4 slashes
 qa['fname'] = [re.sub('.*/([^/]*/[^/]*/[^/]*/[^/]*)$','\\1',x) for x in qa['video']]
 # Note that if a video was rerun, keep the last entry
@@ -94,28 +35,38 @@ qa = qa.drop_duplicates(subset=['fname'], keep='last').reset_index(drop=True)
 df = pd.merge(df, qa, how='left', on='fname')
 df['time'] = [time.strptime(df.loc[i,'day'] + ' ' + df.loc[i,'time_x'], '%Y-%m-%d %H-%M-%S') for i in range(len(df))]
 df['time'] = df['time'].apply(lambda x: pd.Timestamp(datetime(*x[:6])))
+df['exp_date_timestamp'] = df['exp_date'].apply(lambda x: pd.Timestamp(datetime(*(time.strptime(x, '%Y-%m-%d')[:6]))))
+df['rel_exp_time'] = df['time'] - df['exp_date_timestamp']
 
 # Include metadata for summarizing recommendations
 meta_df = pd.read_excel('/home/bgeuther/Downloads/2023-08-04 TOM_TotalQueryForConfluence.xlsx')
-meta_df = meta_df[['ExptNumber','sex','Strain','Location']].drop_duplicates()
-meta_df['Room'] = [x.split(' ')[0] if isinstance(x,str) else ''  for x in meta_df['Location']]
-meta_df['Computer'] = [re.sub('.*(NV[0-9]+).*','\\1',x) if isinstance(x,str) else ''  for x in meta_df['Location']]
-meta_df['ExptCleaned'] = [re.sub('.*(MD[XB][0-9]+).*','\\1',x) for x in meta_df['ExptNumber']]
+meta_df = meta_df[['ExptNumber', 'sex', 'Strain', 'Location']].drop_duplicates()
+meta_df['Room'] = [x.split(' ')[0] if isinstance(x, str) else '' for x in meta_df['Location']]
+meta_df['Computer'] = [re.sub('.*(NV[0-9]+).*', '\\1', x) if isinstance(x, str) else '' for x in meta_df['Location']]
+meta_df['ExptCleaned'] = [re.sub('.*(MD[XB][0-9]+).*', '\\1', x) for x in meta_df['ExptNumber']]
 df = pd.merge(df, meta_df, left_on='exp', right_on='ExptCleaned', how='left')
 
 # Plot for QC
+# (
+#     p9.ggplot(df, p9.aes(x='time', y='avg_longtermid_count', color='dataset', shape='Strain'))
+#     + p9.geom_point()
+#     + p9.facet_wrap('~exp', scales='free_x')
+#     + p9.theme_bw()
+# ).draw().show()
+
 (
-    p9.ggplot(df, p9.aes(x='time', y='avg_longtermid_count', color='dataset', shape='Strain')) + 
-    p9.geom_point() +
-    p9.facet_wrap('~exp', scales='free_x') +
-    p9.theme_bw()
+    p9.ggplot(df, p9.aes(x='rel_exp_time', y='avg_longtermid_count', color='exp', shape='Strain'))
+    # p9.geom_line() +
+    + p9.geom_point()
+    + p9.facet_grid('dataset~.')
+    + p9.theme_bw()
 ).draw().show()
 
 groupings = df.groupby(['exp','Strain','sex','dataset'])
 quality_df = groupings.agg({'avg_longtermid_count': np.mean, 'hour':len}).reset_index()
 
-# Set a really high bar of <6 hours in the 4-day experiment missing.
-low_count = quality_df['exp'][quality_df['hour'] < 90].to_list()
+# Set a really high bar of 10% missing data in the 4-day experiment.
+low_count = quality_df['exp'][quality_df['hour'] < 86].to_list()
 
 def print_quality_subset_summary(quality_df, threshold):
     subset_df = quality_df[quality_df['avg_longtermid_count']>threshold]
@@ -125,8 +76,8 @@ def print_quality_subset_summary(quality_df, threshold):
     print(subset_df.groupby(['Strain','sex','dataset']).apply(len).reset_index())
     return removed_df.to_list()
 
-low_quality = print_quality_subset_summary(quality_df, 2.5)
-low_quality_2 = print_quality_subset_summary(quality_df, 2.0)
+low_quality = print_quality_subset_summary(quality_df[~np.isin(quality_df['exp'],np.array(low_count))], 2.5)
+low_quality_2 = print_quality_subset_summary(quality_df[~np.isin(quality_df['exp'],np.array(low_count))], 2.0)
 
 experiments_to_remove = np.unique(low_count + low_quality)
 experiments_to_remove_2 = np.unique(low_count + low_quality_2)
@@ -135,3 +86,12 @@ experiments_to_remove_2 = np.unique(low_count + low_quality_2)
 df['fname'].to_csv(folder + time.strftime('%Y-%m-%d') + '_poses.txt', header=False, index=False)
 np.savetxt(folder + time.strftime('%Y-%m-%d') + '_failed_qc.txt', experiments_to_remove, fmt='%s')
 np.savetxt(folder + time.strftime('%Y-%m-%d') + '_failed_qc_lenient.txt', experiments_to_remove_2, fmt='%s')
+
+missing_lixit = df['fname'][np.logical_or(np.logical_or(~df['lixit_side_check'].astype(bool), ~df['lixit_location_check'].astype(bool)), df['num_lixit'] != 1)].values
+missing_food = df['fname'][~df['food_detected'].astype(bool)].values
+missing_corners = df['fname'][~df['arena_detected'].astype(bool)].values
+missing_static_objects = np.unique(np.concatenate([missing_lixit, missing_food, missing_corners]))
+missing_non_lixit = np.unique(np.concatenate([missing_food, missing_corners]))
+
+np.savetxt(folder + time.strftime('%Y-%m-%d') + '_missing_static_objs.txt', missing_static_objects, fmt='%s')
+np.savetxt(folder + time.strftime('%Y-%m-%d') + '_missing_non_lixit.txt', missing_non_lixit, fmt='%s')
