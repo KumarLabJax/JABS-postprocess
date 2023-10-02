3b37f7b446fd8244e0b97ab17fa355c168be914d
Brian Geuther <Brian.Geuther@jax.org>
2023-10-02 14:52:39 -0400

    Simplifying some phase data wrangling

--- phase_single_behavior.py
+++ phase_single_behavior.py
@@ -69,47 +69,19 @@ def run_analysis(args):
     ).save('freq_amp.png')
 
     #Filter out the frequencies below or above the allowed range
-    filtered_phase_df = phase_df
-    for instance in phase_df.index: 
-        freq_list = phase_df['freq'][instance]
-        mask = np.ones(len(freq_list), dtype=bool)
-        for freq in freq_list: 
-            if freq >= 1/22 or freq <= 1/25: 
-                index = phase_df['freq'][instance].tolist().index(freq)
-                mask[index] = False
-        filtered_phase_df['freq'][instance] = freq_list[mask,...]
-        filtered_phase_df['amplitude'][instance] = filtered_phase_df['amplitude'][instance][mask,...]
-        filtered_phase_df['period'][instance] = filtered_phase_df['period'][instance][mask,...]
-        filtered_phase_df['phase'][instance] = filtered_phase_df['phase'][instance][mask,...]
+    filtered_phase_df = phase_df[phase_df['freq'].between(1/25, 1/22)]
 
     # Find dominant period by taking the period associated with the max amplitude
-    period_df = pd.DataFrame(columns=['mouse_id', 'dominant_period', 'amplitude', 'strain'])
-    ray_df = pd.DataFrame(columns=['mouse_id', 'peak_phase', 'amplitude', 'strain'])
-    for mouse in phase_df.index: 
-        amp_list = phase_df['amplitude'][mouse].tolist()
-        if type(amp_list) == float:
-            max_amp = amp_list
-            dom_period = phase_df['period'][mouse].tolist()
-            peak_phase = phase_df['period'][mouse].tolist()
-        else:
-            max_amp = max(amp_list)
-            max_index = amp_list.index(max_amp)
-            dom_period = phase_df['period'][mouse][max_index]
-            peak_phase = phase_df['phase'][mouse][max_index]
-        tmp = {'mouse_id': mouse, 'dominant_period': dom_period, 'amplitude': max_amp, 'strain': phase_df['strain'][mouse]}
-        tmp2 = {'mouse_id': mouse, 'peak_phase': peak_phase, 'amplitude': max_amp, 'strain': phase_df['strain'][mouse]}
-        tmp = pd.DataFrame([tmp])
-        tmp2 = pd.DataFrame([tmp2])
-        period_df = pd.concat([period_df, tmp])
-        ray_df = pd.concat([ray_df, tmp2])
+    peak_df = filtered_phase_df.groupby('group').apply(lambda x: x.iloc[np.argmax(x['amplitude'])]).reset_index(drop=True)
 
     # Plot the dominant periods
-    (p9.ggplot(period_df, p9.aes(x='dominant_period')) + 
-        p9.geom_histogram(binwidth=0.5, fill=BLUE, color='black') + 
-        p9.theme_bw() + 
-        p9.labs(x='Dominant Period', y='Count', title='Dominant Period Comparison') + 
-        p9.theme(title = p9.element_text(hjust=0.5))
-        ).save('period_hist.png')
+    (
+        p9.ggplot(peak_df, p9.aes(x='period'))
+        + p9.geom_histogram(binwidth=0.5, fill=BLUE, color='black')
+        + p9.theme_bw()
+        + p9.labs(x='Dominant Period', y='Count', title='Dominant Period Comparison')
+        + p9.theme(title=p9.element_text(hjust=0.5))
+    ).save('period_hist.png')
 
     #=== SHOW THE PHASE SYNCHRONY ===#
 
@@ -121,22 +93,22 @@ def run_analysis(args):
     def cart2pol(x, y):
         rho = np.sqrt(x**2 + y**2)
         phi = np.arctan2(y, x)
-        return(rho, phi)
+        return (rho, phi)
 
-    # Put the phase values on a scale of 0 to 2pi for plotting 
-    ray_df['peak_phase'] = ray_df['peak_phase'] % (2*np.pi)
     # Shift the phase values by pi/2 to show the peak time of eating rather than when they begin to move towards their peak time of eating
-    ray_df['peak_phase'] = ray_df['peak_phase'] + (np.pi)/2
-    ray_df['r'] = 1
-    ray_df['x'], ray_df['y'] = pol2cart(ray_df['r'], ray_df['peak_phase'])
+    peak_df['phase'] = peak_df['phase'] + (np.pi) / 2
+    # Put the phase values on a scale of 0 to 2pi for plotting
+    peak_df['phase'] = peak_df['phase'] % (2 * np.pi)
+    peak_df['r'] = 1
+    peak_df['x'], peak_df['y'] = pol2cart(peak_df['r'], peak_df['phase'])
 
-    b6_df = ray_df[ray_df['strain'] == 'C57BL/6J']
-    btbr_df = ray_df[ray_df['strain'] == 'BTBR T<+> ltpr3<tf>/J']
+    b6_df = peak_df[peak_df['strain'] == 'C57BL/6J']
+    btbr_df = peak_df[peak_df['strain'] == 'BTBR T<+> ltpr3<tf>/J']
 
-    center_b6x = sum(b6_df['x'])/len(b6_df['x'])
-    center_b6y = sum(b6_df['y'])/len(b6_df['y'])
-    center_btbrx = sum(btbr_df['x'])/len(btbr_df['x'])
-    center_btbry = sum(btbr_df['y'])/len(btbr_df['y'])
+    center_b6x = sum(b6_df['x']) / len(b6_df['x'])
+    center_b6y = sum(b6_df['y']) / len(b6_df['y'])
+    center_btbrx = sum(btbr_df['x']) / len(btbr_df['x'])
+    center_btbry = sum(btbr_df['y']) / len(btbr_df['y'])
 
     r_b6, theta_b6 = cart2pol(center_b6x, center_b6y)
     r_btbr, theta_btbr = cart2pol(center_btbrx, center_btbry)
