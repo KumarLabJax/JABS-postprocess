b3334c47971f62a99c86e46ee0d04c02836c1f2b
Brian Geuther <Brian.Geuther@jax.org>
2024-07-08 11:40:23 -0400

    Added back in event -> summary tables.

--- jabs_utils/project_utils.py
+++ jabs_utils/project_utils.py
@@ -434,10 +434,28 @@ class BoutTable(Table):
 		self._optional_columns = ['longterm_idx', 'exp_prefix', 'time', 'distance', 'closest_id', 'closest_lixit', 'closest_corner']
 		self._check_fields()
 
-	def to_summary_table(self, bin_size):
-		"""Converts bout information into binned summary table."""
-		# TODO: Carry over the time-binning summary code...
-		raise NotImplementedError()
+	def to_summary_table(self, bin_size_minutes: int = 60):
+		"""Converts bout information into binned summary table.
+
+		Args:
+			bin_size_minutes: size of the bin in minutes
+
+		Returns:
+			BinTable representation of this data
+		"""
+		if 'longterm_idx' not in self.data.keys():
+			self.data['longterm_idx'] = 1
+		else:
+			added_dummy_longterm = False
+		grouped_df = self.data.groupby(['exp_prefix', 'longterm_idx'])
+		all_results = []
+		for cur_group, cur_data in grouped_df:
+			binned_results = self.bouts_to_bins(cur_data, bin_size_minutes)
+			binned_results['exp_prefix'], binned_results['longterm_idx'] = cur_group
+			all_results.append(binned_results)
+		all_results = pd.concat(all_results)
+
+		return BinTable(self._settings, all_results)
 
 	def compare_to(self, other: BoutTable, state: int = 1):
 		"""Compares these bouts with other bouts.
@@ -454,19 +472,137 @@ class BoutTable(Table):
 		"""
 		raise NotImplementedError()
 
+	@staticmethod
+	def bouts_to_bins(event_df: pd.DataFrame, bin_size_minutes: int = 60, fps: int = 30):
+		"""Converts bout data to bin data.
+
+		Args:
+			event_df: data frame to be converted
+			bin_size_minutes: duration of the bin
+			fps: frames per second to convert frame data into bins
+
+		Returns:
+			Binned event data describing the event data.
+
+		Notes:
+			Binned data describes event data as summaries. For each state, total time and distance travelled are provided. Additionally, the number of behavior events are counted.
+			Events that span multiple bins are split between them based on the percent in each, allowing fractional bout counts.
+		""" 
+		# Get the range that the experiment spans
+		try:
+			# TODO: Add support for different sized experiment blocks (re-use block below to make an end time that is adjusted per-video)
+			start_time = BoutTable.round_hour(datetime.strptime(min(event_df['time']), '%Y-%m-%d %H:%M:%S'))
+			end_time = BoutTable.round_hour(datetime.strptime(max(event_df['time']), '%Y-%m-%d %H:%M:%S'), up=True)
+		# Timestamp doesn't exist. Make up some. This assumes only 1 video exists and just makes up timestamps based on the available bout data.
+		except:
+			start_time_str = '1970-01-01 00:00:00'
+			start_time = BoutTable.round_hour(datetime.strptime(start_time_str, '%Y-%m-%d %H:%M:%S'))
+			num_hours = np.max(event_df['start'] + event_df['duration']) / fps / 60 // 60 + 1
+			end_time_str = start_time_str
+			for _ in np.arange(num_hours):
+				end_time = BoutTable.round_hour(datetime.strptime(end_time_str, '%Y-%m-%d %H:%M:%S'), up=True)
+				end_time_str = str(end_time)
+			# Add one last hour (can be discarded later)
+			end_time = BoutTable.round_hour(datetime.strptime(end_time_str, '%Y-%m-%d %H:%M:%S'), up=True)
+			# Also adjust the df to contain valid times
+			event_df['time'] = start_time_str
+		# Calculate the framewise time bins that we need to summarize
+		time_idx = pd.date_range(start=start_time, end=end_time, freq=str(bin_size_minutes) + 'min')
+		event_df['adjusted_start'] = [BoutTable.time_to_frame(x['time'], str(start_time), 30) + x['start'] for row_idx, x in event_df.iterrows()]
+		event_df['adjusted_end'] = event_df['adjusted_start'] + event_df['duration']
+		event_df['percent_bout'] = 1.0
+		# Summarize each time bin
+		results_df_list = []
+		for t1, t2 in zip(time_idx[:-1], time_idx[1:]):
+			start_frame = BoutTable.time_to_frame(str(t1), str(start_time), fps)
+			end_frame = BoutTable.time_to_frame(str(t2), str(start_time), fps)
+			bins_to_summarize = event_df[np.logical_and(event_df['adjusted_start'] >= start_frame, event_df['adjusted_end'] <= end_frame)]
+			cut_start = pd.DataFrame.copy(event_df[np.logical_and(event_df['adjusted_start'] < start_frame, event_df['adjusted_end'] >= start_frame)])
+			if len(cut_start) > 0:
+				new_duration = cut_start['duration'] - (cut_start['adjusted_start'] - start_frame)
+				cut_start['percent_bout'] = new_duration / cut_start['duration']
+				cut_start['duration'] = new_duration
+				bins_to_summarize = pd.concat([bins_to_summarize, cut_start])
+			cut_end = pd.DataFrame.copy(event_df[np.logical_and(event_df['adjusted_start'] < end_frame, event_df['adjusted_end'] >= end_frame)])
+			if len(cut_end) > 0:
+				new_duration = cut_end['duration'] - (end_frame - cut_end['adjusted_start'])
+				cut_end['percent_bout'] = new_duration / cut_end['duration']
+				cut_end['duration'] = new_duration
+				bins_to_summarize = pd.concat([bins_to_summarize, cut_end])
+			# With bins_to_summarize as needed
+			# This operation throws a warning which can be ignored, so mute it before throwing...
+			pd.options.mode.chained_assignment = None
+			if 'distance' in bins_to_summarize.keys():
+				bins_to_summarize['calc_dist'] = bins_to_summarize['distance'] * bins_to_summarize['percent_bout']
+			else:
+				pass
+			pd.options.mode.chained_assignment = 'warn'
+			results = {}
+			results['time'] = [str(t1)]
+			results['time_no_pred'] = bins_to_summarize.loc[bins_to_summarize['is_behavior'] == -1, 'duration'].sum()
+			results['time_not_behavior'] = bins_to_summarize.loc[bins_to_summarize['is_behavior'] == 0, 'duration'].sum()
+			results['time_behavior'] = bins_to_summarize.loc[bins_to_summarize['is_behavior'] == 1, 'duration'].sum()
+			results['bout_behavior'] = len(bins_to_summarize.loc[bins_to_summarize['is_behavior'] == 1] )
+			if 'distance' in bins_to_summarize.keys():
+				results['not_behavior_dist'] = bins_to_summarize.loc[bins_to_summarize['is_behavior'] == 0, 'calc_dist'].sum()
+				results['behavior_dist'] = bins_to_summarize.loc[bins_to_summarize['is_behavior'] == 1, 'calc_dist'].sum()
+			results_df_list.append(pd.DataFrame(results))
+
+		# Remove an non-informative rows
+		results = pd.concat(results_df_list)
+		no_frames = (results['time_no_pred'] + results['time_not_behavior'] + results['time_behavior'] == 0).values
+		results = results[~no_frames]
+		return results.reset_index(drop=True)
+
+	@staticmethod
+	def round_hour(t, up: bool = False):
+		"""Rounds a time to the hour.
+
+		Args:
+			t: time object to round
+			up: should time be rounded up (True) or down (False)
+
+		Returns:
+			time object rounded to the hour
+		"""
+		hour_to_round_to = t.hour
+		# If we want to round up, just increment the hour
+		if up:
+			hour_to_round_to += 1
+		# Do some remainder/modulo operations to handle day wrapping
+		return (t.replace(day=t.day + (hour_to_round_to)//24, second=0, microsecond=0, minute=0, hour=hour_to_round_to%24))
+
+	@staticmethod
+	def time_to_frame(t: str, rel_t: str, fps: float):
+		"""Converts a time relative to another to a frame index.
+
+		Args:
+			t: time of interest
+			rel_t: time relative to t (typically recording start time)
+			fps: frames per second
+
+		Returns:
+			frame count difference for t - rel_t
+		"""
+		delta = datetime.strptime(t, '%Y-%m-%d %H:%M:%S') - datetime.strptime(rel_t, '%Y-%m-%d %H:%M:%S')
+		return np.int64(delta.total_seconds() * fps)
+
 
 class BinTable(Table):
 	"""Object that handles time-binned data."""
-	def __init(self, settings: ClassifierSettings, data: pd.DataFrame):
+	def __init__(self, settings: ClassifierSettings, data: pd.DataFrame):
 		"""Initializes a binned object.
 
 		Args:
 			settings: settings used for these bins
 			data: pandas dataframe containing the binned data
 		"""
-		super().__init__(settings, data)
-		self._required_columns = ['animal_idx', 'video_name', 'time_no_pred', 'time_not_behavior', 'time_behavior', 'bout_behavior']
-		self._optional_columns = ['longterm_idx', 'exp_prefix', 'time', 'not_behavior_dist', 'behavior_dist']
+		# Skip passing the data in because it will fail the column check and instead place it in afterwards
+		super().__init__(settings, None)
+		self._data = data
+		self._required_columns = ['longterm_idx', 'time_no_pred', 'time_not_behavior', 'time_behavior', 'bout_behavior']
+		self._optional_columns = ['animal_idx', 'exp_prefix', 'time', 'not_behavior_dist', 'behavior_dist']
+		self._check_fields()
 
 
 class Prediction(BoutTable):
